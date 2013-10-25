4549527dd5f6bf684e794665d5f3302681c3c247
root <root@hadoop.main>
2013-10-25 10:26:11 +0800

    wordcount

---
+++ src/WordCount.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class WordCount {
+	
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable>
+	{
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			
+			while(tokenizer.hasMoreElements()){
+				word.set(tokenizer.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+	
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>
+	{
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+		{
+			int i = 0;
+			for(IntWritable value : values)
+			{
+				i++;
+			}
+			context.write(key, new IntWritable(i));
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+		
+		Job job = new Job();
+		job.setJarByClass(WordCount.class);
+		job.setJobName("word count demo");
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
